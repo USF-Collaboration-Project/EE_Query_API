8b5b0a57dbfa5b16863d06cfb88a7752d267327c
Nathan Pillai <natepill@gmail.com>
2019-09-21 09:35:14 -0700

    [added] TODO comments to parse_data script file

---
+++ parse_data.py
@@ -0,0 +1,6 @@
+# gadm36_USA_0_sp.rds
+
+# TODO:
+# - Parse State/County GAMD level 2 500k into JSON, CSV, or GoeJSON
+# - Pickle objects and store in a folder hierarchy: (coords --> states ---> county)
+# - Process form submission to make GEE query based on unpickled coordinates 
