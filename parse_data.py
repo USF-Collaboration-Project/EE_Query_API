f3a002ccce0150044b0e33caf4783bb906659005
Nathan Pillai <natepill@gmail.com>
2019-09-22 13:57:21 -0700

    [added] comments and file organization into functions and main script

--- parse_data.py
+++ parse_data.py
@@ -1,4 +1,3 @@
-# gadm36_USA_0_sp.rds
 
 """
     Geospatial data is being obtained from https://gadm.org/index.html
@@ -19,82 +18,102 @@
 """
 
 # TODO:
-# - Parse State/County GAMD level 2 500k into JSON, CSV, or GoeJSON
-# - Pickle objects and store in a folder hierarchy: (coords --> states ---> county)
-# - Process form submission to make GEE query based on unpickled coordinates 
+# - Parse State/County GAMD level 2 500k into JSON, CSV, or GoeJSON (DONE)
+# - Pickle objects and store in a folder hierarchy: (coords --> states ---> county) (DONE)
+# - Process form submission to make GEE query based on unpickled coordinates (Need to do on server)
 
 import json
 import pickle
 import os
 
-# with open('gadm36_USA_1.json', 'r') as file:
-#     data = file.read()
-#
-# # parse string to json
-# obj = json.loads(data)
+def parse_state_geojson():
+    """
+        Script to parse GeoJSON county shapefile and cached the file to server directory
 
-# Prints name of State
-# print(obj["features"][0]["properties"]["NAME_1"])
-# # Prints coordinates for State
-# print(obj["features"][0]["geometry"])
-# # Prints properties of an coordinate object
-# print(obj["features"][0]["properties"])
+        Args: None
+        Return: None
+    """
 
+    # Read geojson to string
+    with open('gadm36_USA_1.json', 'r') as file:
+        data = file.read()
 
-# testing with one state obj
-# state_data = obj["features"]
-#
-# # TODO: create state_name:state_coords for each object. Pickle object. Store object in coords/state file
-# # https://docs.python.org/3/library/pickle.
-# for state in state_data:
-#     state_name = state["properties"]["NAME_1"]
-#     state_coords = state["geometry"]
-#
-#     with open(f'coords/states/{state_name}', 'wb') as geo_file:
-#         state_obj = {state_name:state_coords}
-#         pickle.dump(state_obj, geo_file)
+    # parse string to json
+    obj = json.loads(data)
 
+    # Prints name of State
+    # print(obj["features"][0]["properties"]["NAME_1"])
+    # Prints coordinates for State
+    # print(obj["features"][0]["geometry"])
+    # Prints properties of an coordinate object
+    # print(obj["features"][0]["properties"])
 
-with open('gadm36_USA_2.json', 'r') as file:
-    data = file.read()
+    # Static json path to features
+    state_data = obj["features"]
 
-# parse string to json
-obj = json.loads(data)
+    # Parse JSON to seperate state data into seperate files
+    for state in state_data:
+        # Extract state name
+        state_name = state["properties"]["NAME_1"]
+        # Extract state geometry
+        state_coords = state["geometry"]
 
-
-county_data = obj["features"]
-
-for county in county_data:
-    state_name = county["properties"]["NAME_1"]
-    county_name = county["properties"]["NAME_2"]
-    county_coords = county["geometry"]
-
-    state_file_path = f'coords/counties/{state_name}'
-    county_file_path = f'coords/counties/{state_name}/{county_name}'
-
-    if not os.path.exists(state_file_path):
-        os.makedirs(state_file_path)
-
-    with open(county_file_path, 'wb') as geo_file:
-        county_obj = {county_name:county_coords}
-        pickle.dump(county_obj, geo_file)
+        # Write binary file for specific state and cache the file
+        with open(f'coords/states/{state_name}', 'wb') as geo_file:
+            state_obj = {state_name:state_coords}
+            pickle.dump(state_obj, geo_file)
 
 
+def parse_county_geojson():
+    """
+        Script to parse GeoJSON county shapefile and cached the file to server directory
+
+        Args: None
+        Return: None
+    """
+
+    # Read geojson to string
+    with open('gadm36_USA_2.json', 'r') as file:
+        data = file.read()
+
+    # parse string to json
+    obj = json.loads(data)
+
+    # Static json path to features
+    county_data = obj["features"]
+
+    # Parse county json to seperate cached files
+    for county in county_data:
+        # Extract State and County name
+        state_name = county["properties"]["NAME_1"]
+        county_name = county["properties"]["NAME_2"]
+        # Extact county geomety
+        county_coords = county["geometry"]
+
+        # Static formated file paths
+        state_file_path = f'coords/counties/{state_name}'
+        county_file_path = f'coords/counties/{state_name}/{county_name}'
+
+        # If state directory doesn't exist, create it
+        # Seperating county data into state folders in case there are counties in different
+        # states with the same name
+        if not os.path.exists(state_file_path):
+            os.makedirs(state_file_path)
+
+        # Write binary file for specific county and cache the file
+        with open(county_file_path, 'wb') as geo_file:
+            county_obj = {county_name:county_coords}
+            pickle.dump(county_obj, geo_file)
 
 
+if __name__ == "__main__":
 
+    # NOTE: Uncomment to parse state/county GeoJSON shape files
+    # parse_state_geojson()
+    # parse_county_geojson()
 
-
-# NOTE: Use this code for the API
-# with open(f'coords/states/{state_name}', 'rb') as pickle_file:
-#     geo_json = pickle.load(pickle_file)
-#     print(geo_json)
-
-
-
-
-
-
-# {'GID_0': 'USA', 'NAME_0': 'United States', 'GID_1': 'USA.1_1', 'NAME_1': 'Alabama', 'VARNAME_1':
-# 'AL|Ala.', 'NL_NAME_1': '', 'TYPE_1': 'State', 'ENGTYPE_1': 'State', 'CC_1': '', 'HASC_1': 'US.AL'
-# }
+    # NOTE: MOVE TO MAIN SERVER FILE
+    # Read cached GeoJSON
+    # with open(f'coords/states/{state_name}', 'rb') as pickle_file:
+    #     geo_json = pickle.load(pickle_file)
+    #     print(geo_json)
