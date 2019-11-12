fcba86df0c3c643eeb0daf96d86bb2a5f5d02e86
Nathan Pillai <natepill@gmail.com>
2019-11-11 17:06:40 -0800

    [edit] config vars

--- ee_config.py
+++ ee_config.py
@@ -5,7 +5,7 @@ import os
 EE_ACCOUNT = os.environ['GEE_SERVICE_ACCOUNT']
 
 # The private key associated with service account in JSON format.
-EE_PRIVATE_KEY_FILE = os.environ['GEE_KEY_FILE']
+EE_PRIVATE_KEY_FILE = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
 
 # Authorize connection to EE
 EE_CREDENTIALS = ee.ServiceAccountCredentials(EE_ACCOUNT, EE_PRIVATE_KEY_FILE)
